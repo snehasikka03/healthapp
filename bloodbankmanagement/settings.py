7e2cce5ee8735c83569e7bebde4aa33c8c4846d5
snehasikka03 <snehasikka03@gmail.com>
2024-01-21 00:34:39 +0530

    update settings

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -124,9 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
+#STATICFILES_DIRS=[
+#STATIC_DIR,
+ #]
+STATIC_ROOT = os.join(BASE_DIR,'staticfiles')
 LOGIN_REDIRECT_URL='/afterlogin'
 
 #for contact us give your gmail id and password
