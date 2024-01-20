50f9c06d949487d59eef988cbae5e73c1b2de46a
snehasikka03 <snehasikka03@gmail.com>
2024-01-21 04:27:10 +0530

    u

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -135,6 +135,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+DEBUG = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
@@ -153,7 +154,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     }
 }
 
